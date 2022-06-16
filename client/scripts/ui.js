80177c5011271400520776050befc49b25b4f10d
Matheus Breguêz <matbrgz@mail.com>
2022-06-16 01:44:43 -0300

    .

--- client/scripts/ui.js
+++ client/scripts/ui.js
@@ -76,7 +76,7 @@ class PeerUI {
             <label class="column center m-2" title="Click to send files or right click to send a text">
                 <input type="file" multiple>
                 <div role="button" class="btn btn-primary btn-circle btn-xl position-relative mb-1 icon"></div>
-                <div class="progress-circle">
+                <div class="progress-circle position-absolute">
                   <div class="circle"></div>
                   <div class="circle right"></div>
                 </div>
